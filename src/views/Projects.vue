c8a7dbb2b7183576eb935db810067e5226a88214
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2025-06-02 17:12:34 +0500

    Add or update src/views/Projects.vue

---
+++ src/views/Projects.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h2>Лоиҳаҳои ман</h2>
+    <div class="projects-list">
+      <ProjectCard v-for="project in data.projects" :key="project.title" :project="project" />
+    </div>
+  </section>
+</template>
+
+<script setup>
+import data from '../assets/data.json'
+import ProjectCard from '../components/ProjectCard.vue'
+</script>
