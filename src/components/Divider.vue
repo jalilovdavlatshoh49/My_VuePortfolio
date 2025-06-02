fa6f2ccce0131ec780b74589d4b4bdb9e027aa31
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2025-06-02 23:41:11 +0500

    Add or update src/components/Divider.vue

---
+++ src/components/Divider.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="divider"></div>
+</template>
+
+<style scoped>
+.divider {
+  width: 92%;
+  margin: 18px auto;
+  height: 2px;
+  background: linear-gradient(90deg, transparent 2%, #5686e0 49%, transparent 98%);
+  opacity: 0.16;
+  border-radius: 1px;
+  box-shadow: 0 2px 8px #5686e022;
+}
+</style>
