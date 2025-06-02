66a90dccb544805d7cc5910367cb1a553a16aeb8
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2025-06-02 23:41:04 +0500

    Add or update src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <!-- Блок профиля -->
+    <ProfileCard />
+    <Divider />
+
+    <!-- Блок знаний -->
+    <KnowledgeCard />
+    <Divider />
+
+    <!-- Блок проектов -->
+    <ProjectsCard />
+  </div>
+</template>
+
+<script setup>
+// Импортируем компоненты
+import ProfileCard from './components/ProfileCard.vue'
+import KnowledgeCard from './components/KnowledgeCard.vue'
+import ProjectsCard from './components/ProjectsCard.vue'
+import Divider from './components/Divider.vue'
+</script>
