40cad5634c4216dbf33d314bf17971f5b3034c42
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2025-06-02 17:12:26 +0500

    Add or update src/views/Skills.vue

---
+++ src/views/Skills.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Маҳоратҳои ман</h2>
+    <ul>
+      <li v-for="skill in data.skills" :key="skill">{{ skill }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import data from '../assets/data.json'
+</script>
