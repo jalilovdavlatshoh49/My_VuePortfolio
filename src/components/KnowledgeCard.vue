3cb3b4442d875a32f02e419a33e46e661eb6ffbc
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2025-06-02 23:41:15 +0500

    Add or update src/components/KnowledgeCard.vue

---
+++ src/components/KnowledgeCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="card">
+    <h2>Языки программирования</h2>
+    <ul class="chips">
+      <li v-for="lang in profile.languages" :key="lang">{{ lang }}</li>
+    </ul>
+    <h2>Фреймворки и технологии</h2>
+    <ul class="chips">
+      <li v-for="fw in profile.frameworks" :key="fw">{{ fw }}</li>
+    </ul>
+  </section>
+</template>
+
+<script setup>
+import profile from '../data/profile.json'
+</script>
