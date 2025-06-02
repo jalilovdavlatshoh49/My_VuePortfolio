1f940c10b6250dddb90240f089899ca4a2ae8a60
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2025-06-02 17:12:15 +0500

    Add or update src/components/ProjectCard.vue

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="project-card">
+    <img v-if="project.image" :src="project.image" :alt="project.title" />
+    <h3>{{ project.title }}</h3>
+    <p>{{ project.description }}</p>
+    <a :href="project.link" target="_blank" rel="noopener">[Нишон додан]</a>
+  </div>
+</template>
+
+<script setup>
+defineProps(['project'])
+</script>
