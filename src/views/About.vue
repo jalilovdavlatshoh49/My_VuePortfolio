f2b20db747d9edad3d248a61e3dd3f079b146ed8
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2025-06-02 17:12:24 +0500

    Add or update src/views/About.vue

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h2>Дар бораи ман</h2>
+    <p>{{ data.about }}</p>
+    <ul>
+      <li><strong>Ном:</strong> {{ data.name }}</li>
+      <li><strong>Касб:</strong> {{ data.title }}</li>
+    </ul>
+  </section>
+</template>
+
+<script setup>
+import data from '../assets/data.json'
+</script>
