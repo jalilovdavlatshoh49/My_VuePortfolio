4f42c462ec99f420bfe296d07c858d46073c7ef8
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2025-06-02 23:41:17 +0500

    Add or update src/components/ProjectsCard.vue

---
+++ src/components/ProjectsCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="card">
+    <h2>Проекты на GitHub</h2>
+    <div class="projects">
+      <div v-for="project in profile.projects" :key="project.title" class="project">
+        <a :href="project.link" target="_blank" class="project-title">{{ project.title }}</a>
+        <p class="project-desc">{{ project.description }}</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import profile from '../data/profile.json'
+</script>
