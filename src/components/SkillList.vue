e6053c8485f92305f0721adfc07b34a02b684ea7
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2025-06-02 17:12:22 +0500

    Add or update src/components/SkillList.vue

---
+++ src/components/SkillList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="skills-list">
+    <div class="skill-card" v-for="skill in skills" :key="skill.name">
+      <i :class="skill.icon"></i>
+      <span>{{ skill.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps(['skills'])
+</script>
